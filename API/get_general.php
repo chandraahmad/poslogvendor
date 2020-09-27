7ef15a5cb8d6188a7149ed2c6e0612ff90fe2466
rizkyghani16 <rizkyghani16@gmail.com>
2020-09-28 01:15:20 +0700

    menambahkan API pekerjaan tricha

---
+++ API/get_general.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once 'koneksi.php';
+
+header('Content-Type: application/json');
+
+$query = "SELECT * FROM general";
+
+$result = mysqli_query($conn, $query);
+
+$array = array();
+
+while ($row = mysqli_fetch_assoc($result))
+{
+	$array[] = $row;
+}
+
+echo ($result) ? json_encode(array('kode' => 1, 'result' => $array)):
+                json_encode(array('kode' => 0, 'pesan' => 'Gagal menemukan kuesioner'));
+
+?>
